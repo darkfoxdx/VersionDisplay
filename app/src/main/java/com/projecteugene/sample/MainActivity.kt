2da8d88538d75817770925cab7f8eb59f3bdc29d
Eugene Low <eugenelow@omesti.com>
2019-07-23 15:54:49 +0800

    Updated sample activity and popup view

--- app/src/main/java/com/projecteugene/sample/MainActivity.kt
+++ app/src/main/java/com/projecteugene/sample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.projecteugene.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import com.projecteugene.versiondisplay.VersionUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        VersionUtil.display(this, Gravity.RIGHT or Gravity.BOTTOM, "Test V Test")
     }
 }
